ff1fa1fbc88ee642ea9f8b8c50b3af77516bc4b9
Ryan Oldenburg <guzba8@gmail.com>
2020-10-27 00:18:05 -0500

    formatting

--- tests/test.nim
+++ tests/test.nim
@@ -3,6 +3,6 @@ import noisy
 let simplex = initSimplex(1988) # 1988 is the random seed, generate this however
 
 for y in 0 ..< 10:
-    for x in 0 .. 10:
-        let value = simplex.value(x, y)
-        assert value >= -1.0 and value <= 1.0
+  for x in 0 .. 10:
+    let value = simplex.value(x, y)
+    assert value >= -1.0 and value <= 1.0
