83a8243e87b76ae0e1fe7923fe09a7fa7889fc9d
Ryan Oldenburg <guzba8@gmail.com>
2020-12-04 01:01:36 -0600

    better benchmarks

--- tests/benchmark.nim
+++ tests/benchmark.nim
@@ -1,4 +1,4 @@
-import noisy, std/monotimes, strformat
+import benchy, noisy, strformat
 
 var simplex = initSimplex(1988) # 1988 is the random seed, generate this however
 simplex.octaves = 3
@@ -7,39 +7,31 @@ simplex.amplitude = 0.2
 simplex.lacunarity = 1.5
 simplex.gain = 4.3
 
-block basic_2d:
-  let start = getMonoTime().ticks
+timeIt "2d 4096x4096":
   var c: float
   for x in 0 ..< 4096:
     for y in 0 ..< 4096:
       c += simplex.value(x, y)
-  let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-  echo &"  4096x4096: {delta:.4f}s [{c}]"
+  keep(c)
 
-block simd_2d:
-  let start = getMonoTime().ticks
+timeIt "2d simd 4096x4096":
   var c: float
   let g = simplex.grid((0, 0), (4096, 4096))
   for value in g.values:
     c += value
-  let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-  echo &"  4096x4096: {delta:.4f}s [{c}]"
+  keep(c)
 
-block basic_3d:
-  let start = getMonoTime().ticks
+timeIt "3d 256x256x256":
   var c: float
   for x in 0 ..< 256:
     for y in 0 ..< 256:
       for z in 0 ..< 256:
         c += simplex.value(x, y, z)
-  let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-  echo &"  256x256x256: {delta:.4f}s [{c}]"
+  keep(c)
 
-block simd_3d:
-  let start = getMonoTime().ticks
+timeIt "3d simd 256x256x256":
   var c: float
   let g = simplex.grid((0, 0, 0), (256, 256, 256))
   for value in g.values:
     c += value
-  let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-  echo &"  256x256x256: {delta:.4f}s [{c}]"
+  keep(c)
